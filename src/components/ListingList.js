88620506b9ffe14bc587ce654af5a274c75efac0
Michael Closky <93104023+closkm@users.noreply.github.com>
2022-12-17 10:07:47 -0500

    final mvp

--- src/components/ListingList.js
+++ src/components/ListingList.js
@@ -2,10 +2,10 @@ import React from 'react'
 import Listing from './Listing'
 import '../styles/listing.css'
 
-export default function ListingList( {listings, inCart, deleteFromCart, userId} ) {
+export default function ListingList( {listings, inCart, deleteFromCart, userId, addToCart} ) {
   return (
     <div className="list">
-        {listings.map(listing => <Listing key={listing.id} listing={listing} inCart={inCart} deleteFromCart={deleteFromCart} userId={userId}/>)}
+        {listings.map(listing => <Listing key={listing.id} addToCart={addToCart} listing={listing} inCart={inCart} deleteFromCart={deleteFromCart} userId={userId}/>)}
     </div>
   )
 }
